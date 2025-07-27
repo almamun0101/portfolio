e74d2d1cf42f9d2ce651dd0588b0b7d508f05f70
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-07-28 03:53:14 +0600

    about

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../component/Navbar";
 import Header from "../component/Header";
 import About from "../component/About";
+import Projects from "../component/Projects";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
       <About />
+      <Projects/>
     </div>
   );
 };
--- src/app/component/Projects.jsx
+++ src/app/component/Projects.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+        <div className="">
+            <h2 className='p-10 text-gold text-2xl font-bold text-center'>My Projects</h2>
+            <div className="">
+                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
+                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
