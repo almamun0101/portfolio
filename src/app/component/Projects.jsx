c1172fd7e4ec2758d2a17d0481059e8acd75d3a5
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-07-30 01:48:08 +0600

    project

--- src/app/component/Projects.jsx
+++ src/app/component/Projects.jsx
@@ -5,7 +5,19 @@ const Projects = () => {
     <div>
         <div className="">
             <h2 className='p-10 text-gold text-2xl font-bold text-center'>My Projects</h2>
-            <div className="">
+            <div className="grid grid-cols-2 ">
+                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
+                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
+                </div>
+                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
+                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
+                </div>
+                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
+                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
+                </div>
+                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
+                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
+                </div>
                 <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
                     <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
                 </div>
