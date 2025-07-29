e8e52cc197ef2dfa1e1db226e9f7f2c0be608971
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-07-30 02:02:05 +0600

    project map

--- src/app/component/Projects.jsx
+++ src/app/component/Projects.jsx
@@ -1,30 +1,57 @@
-import React from 'react'
+"use client";
+import React from "react";
 
+const project = [
+  {
+    name: " Project !",
+    cat: "e-comerce",
+    based: "react",
+    github: "jhdsjsdjsjds",
+    live: "sjjhcss",
+  },
+  {
+    name: " Project !",
+    cat: "e-comerce",
+    based: "react",
+    github: "jhdsjsdjsjds",
+    live: "sjjhcss",
+  },
+  {
+    name: " Project !",
+    cat: "e-comerce",
+    based: "react",
+    github: "jhdsjsdjsjds",
+    live: "sjjhcss",
+  },
+  {
+    name: " Project !",
+    cat: "e-comerce",
+    based: "react",
+    github: "jhdsjsdjsjds",
+    live: "sjjhcss",
+  },
+];
 const Projects = () => {
   return (
     <div>
-        <div className="">
-            <h2 className='p-10 text-gold text-2xl font-bold text-center'>My Projects</h2>
-            <div className="grid grid-cols-2 ">
-                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
-                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
-                </div>
-                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
-                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
-                </div>
-                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
-                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
-                </div>
-                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
-                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
-                </div>
-                <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
-                    <h3 className='text-gold text-xl font-semibold'>Project 1</h3>
-                </div>
+      <div className="">
+        <h2 className="p-10 text-gold text-2xl font-bold text-center">
+          My Projects
+        </h2>
+        <div className="grid grid-cols-2 ">
+          {project.map((p) => (
+            <div className="border-2 bg-white/50 border-gold rounded-lg p-5 m-10 hover:bg-white/70 hover:text-yellow-500 duration-300">
+              <h3 className="text-gold text-xl font-semibold">Project 1</h3>
+              <div className="w-full flex justify-between px-2 pt-5">
+                <a href="">Git Code</a>
+                <a href="">Live Preview</a>
+              </div>
             </div>
+          ))}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
