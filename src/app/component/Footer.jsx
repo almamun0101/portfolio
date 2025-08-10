bb7d0b5b9566fdaf72409ea1e03e7c4af6691c5d
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-11 01:24:01 +0600

    footer responsive addded

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -3,13 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <div className="container ">
+      <div className="container p-5 ">
         <div className="text-black font-bold text-xl bg-gold mt-20 mb-5 rounded-full flex justify-between items-center w-full p-5">
           <h2>AL Mamun khan</h2>
           <p className="text-xs lg:text-base  font-light">
             Design and Build By @Al Mamun Khan
           </p>
         </div>
+        <h2 className="text-center py-1">
+
+        @almamunkhan
+        </h2>
       </div>
     </div>
   );
